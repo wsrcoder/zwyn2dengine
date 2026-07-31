338a174d5372369d606c8171ac9a9ac7b00bbab6
Wellington de Sousa Reis <wsr.coder@gmail.com>
2026-07-31 02:27:33 -0300

    (refat)padronização de projectParams

--- editor/src/Constants/ProjectParams.js
+++ editor/src/Constants/ProjectParams.js
@@ -1,17 +1,19 @@
 
 export const ProjectParams = Object.freeze({
-    MAX_MAP_INTERVAL: 4, //Quantas casas o número pode ter
     TILE_SIZE: 32,
     MAP_ORIENTATION: "orthogonal",
     MAP_RENDER_ORDER: "right-down",
 
     // Diretórios principais
-    DATA_DIR: 'Data',
-    SCENES_DIR: 'Data/Scenes',
-    WORLDS_DIR: 'Data/Worlds',
-    ASSETS_DIR: 'Assets',
-    TILESETS_DIR: 'Assets/Tilesets',
-    SPRITES_DIR: 'Assets/Sprites',
+    DIR:{
+        DATA: 'Data',
+        SCENES: 'Data/Scenes',
+        WORLDS: 'Data/Worlds',
+        ASSETS: 'Assets',
+        TILESETS: 'Assets/Tilesets',
+        SPRITES: 'Assets/Sprites',
+    },
+    
     
     // Arquivos de configuração principais
     PROJECT_MANIFEST_FILE: 'project.json'
@@ -19,12 +21,12 @@ export const ProjectParams = Object.freeze({
 
 export const PROJECT_DEFAULTS = Object.freeze({
     DEFAULT_WORLD_NAME: 'Main World',
-    DEFAULT_SCENE_NAME: 'Scene1',
+    DEFAULT_SCENE_NAME: 'Scene',
     DEFAULT_COLUMNS: 20,
     DEFAULT_ROWS: 15,
     
     // Função helper para gerar o nome do arquivo de mapa padronizado
-    getMapFileName(worldId, sceneId) {
+    getSceneFileName(worldId, sceneId) {
         return `W${String(worldId)}S${sceneId}.json`;
     }
 });
